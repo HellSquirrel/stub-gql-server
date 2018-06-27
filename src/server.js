7609d4d08a92a8b64a24032974e94eaf51dab60d
HellSquirrel <zloymult@gmail.com>
2018-06-27 14:27:24 +0300

    add .graphql

--- src/server.js
+++ src/server.js
@@ -1,33 +1,18 @@
 import express from 'express';
 import {graphqlExpress} from 'apollo-server-express';
-import {makeExecutableSchema} from 'graphql-tools';
+import {makeExecutableSchema, addMockFunctionsToSchema} from 'graphql-tools';
 import bodyParser from 'body-parser';
-
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
-
-const typeDefs = `
-  type Query { books: [Book] }
-  type Book { title: String, author: String }
-`;
-
-const resolvers = {
-  Query: {books: () => books},
-};
+import models from './entities/models.graphql';
+import modelResolvers from './entities/models.js';
+const resolvers = {};
 
 // Put together a schema
 const rootSchema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
+  typeDefs: models,
+  resolvers: modelResolvers,
 });
+
+addMockFunctionsToSchema({schema});
 
 const app = express();
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema: rootSchema}));
